6df502eb90d4573d23e2e574ceed41e45f1096ba
Jack Cheng <jack.cheng@commercan.com>
2018-07-08 15:36:50 -0700

    API Info

--- Chapter-11/Chapter-11-account-service/src/main/java/com/dineshonjava/accountservice/config/SwaggerConfig.java
+++ Chapter-11/Chapter-11-account-service/src/main/java/com/dineshonjava/accountservice/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +20,21 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("com.dineshonjava.accountservice.controller"))
+                .paths(PathSelectors.ant("/customer/*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Customer MicroSsrvice REST API",
+                "These are customer service APIs.",
+                "API 2.0",
+                "https://www.dineshonjava.com/Termsofservice",
+                new Contact("Dinesh Rajput", "https://www.dineshonjava.com", "admin@dineshonjava.com"),
+                "License of API",
+                "https://www.dineshonjava.com/license",
+                Collections.emptyList()
+                );
     }
 }
