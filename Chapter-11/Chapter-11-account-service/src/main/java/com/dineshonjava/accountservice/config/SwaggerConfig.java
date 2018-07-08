b4367fadc49eb3a9475a7f514c0fd1ae3b170261
Jack Cheng <jack.cheng@commercan.com>
2018-07-08 15:50:55 -0700

    custom global response message

--- Chapter-11/Chapter-11-account-service/src/main/java/com/dineshonjava/accountservice/config/SwaggerConfig.java
+++ Chapter-11/Chapter-11-account-service/src/main/java/com/dineshonjava/accountservice/config/SwaggerConfig.java
@@ -2,15 +2,22 @@ package com.dineshonjava.accountservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -23,11 +30,27 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.dineshonjava.accountservice.controller"))
                 .paths(PathSelectors.ant("/customer/*"))
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET,
+                        newArrayList(new ResponseMessageBuilder()
+                        .code(500)
+                        .message("500: Internal server error in customer microservice")
+                        .responseModel(new ModelRef("Error"))
+                        .build(),
+                                new ResponseMessageBuilder()
+                        .code(403)
+                        .message("API Request Forbidden!")
+                        .build(),
+                                new ResponseMessageBuilder()
+                        .code(404)
+                        .message("Requested API not found!")
+                        .build()));
+
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo("Customer MicroSsrvice REST API",
+        return new ApiInfo("Customer Microservice REST API",
                 "These are customer service APIs.",
                 "API 2.0",
                 "https://www.dineshonjava.com/Termsofservice",
